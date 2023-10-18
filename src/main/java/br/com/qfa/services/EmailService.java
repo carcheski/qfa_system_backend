c0fbab4a0ba095e78d93e0b5afe57f47fce13617
Pavalo Carcheski <qfa.sistemas@gmail.com>
2023-10-18 14:11:56 -0400

    Email HTML

--- src/main/java/br/com/qfa/services/EmailService.java
+++ src/main/java/br/com/qfa/services/EmailService.java
@@ -3,10 +3,15 @@ package br.com.qfa.services;
 import org.springframework.mail.SimpleMailMessage;
 
 import br.com.qfa.resources.domain.Pedido;
+import jakarta.mail.internet.MimeMessage;
 
 public interface EmailService {
 
 	void sendOrderConfirmationEmail(Pedido obj);
 
 	void sendEmail(SimpleMailMessage msg);
+	
+	void sendOrderConfirmationHtmlEmail(Pedido obj);
+
+	void sendHtmlEmail(MimeMessage msg);
 }
